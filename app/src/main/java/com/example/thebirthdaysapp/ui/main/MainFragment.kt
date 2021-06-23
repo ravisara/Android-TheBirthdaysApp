8cc612356b63543ebf0c619943d0a98df40298ce
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-06-23 11:43:16 +0100

    Adds LiveData so that the results could be observed by the UI.

--- app/src/main/java/com/example/thebirthdaysapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/thebirthdaysapp/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.thebirthdaysapp.R
+import com.example.thebirthdaysapp.viewmodels.MainViewModel
 
 class MainFragment : Fragment() {
 
---
+++ app/src/main/java/com/example/thebirthdaysapp/viewmodels/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.thebirthdaysapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.thebirthdaysapp.helpers.Resource
+import com.example.thebirthdaysapp.repository.BirthdaysDataRepository
+import kotlinx.coroutines.launch
+import com.example.thebirthdaysapp.api.Result
+
+
+class MainViewModel : ViewModel() {
+
+    private val repository = BirthdaysDataRepository()
+
+    private var originalBirthdayResultsFetched: List<Result>? = null
+
+    private val _birthdayResultsResource = MutableLiveData<Resource<List<Result>>>()
+    val birthdayResultsResource: LiveData<Resource<List<Result>>> = _birthdayResultsResource
+
+    fun getBirthdaysAndOtherData() {
+
+        viewModelScope.launch {
+            val originalFetchResult = repository.getBirthdaysAndOtherDataFromAPI()
+            if (originalFetchResult is Resource.Success) { // if the fetch was a success, the original list is saved in the viewmodel
+                originalBirthdayResultsFetched = originalFetchResult.data
+            }
+
+            _birthdayResultsResource.value = originalFetchResult
+
+        }
+
+    }
+
+}
